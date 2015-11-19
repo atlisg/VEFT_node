a572724f7230fddcd2b631af1d77ec5ecef16a87
atlisg12 <atlisg12@ru.is>
2015-11-19 19:53:00 +0000

    DELETE starting up

--- api.js
+++ api.js
@@ -398,28 +398,58 @@ api.post('/companies', bodyParser.json(), (req, res) => {
 
 // Fetching a list of all companies
 api.get('/companies', (req, res) => {
-	
 	const size = req.query.size || 20;
 	const page = req.query.page || 0;
 
 	elasticClient.search({
-			'index': 'punchy',
-			'type':  'companies',
-			'size': size,
-			'from': page,
+			'index'  : 'punchy',
+			'type'   : 'companies',
+			'size'   : size,
+			'from'   : page,
+			"_source": [ "id", "title", "description", "url" ],
 			'body': {
-				'sort': {
-					'title': 'asc'
-				}
+				'sort': [
+					{ 'id': 'asc' }
+				]
 			}
 		}).then((doc) => {
+			console.log('doc:');
+			console.log(doc);
 			res.send(doc.hits.hits.map((d) => d._source));
 		}, (err) => {
 			res.status(500).send('Server error\n');
 		});
 });
 
+// Remove company from mongoDB and ElasticSearch
+api.delete('/companies:id', (req, res) => {
+	// Only admin can delete
+	if (adminToken !== req.headers.token) {
+		res.status(401).send("You don't have authorization to delete a company.\n");
+		return;
+	}
+
+	const cid = req.params.id;
+	models.Company.remove({ _id: cid }, (err, doc) => {
+		if (err) {
+			res.status(500).send('Server error\n');
+			return;
+		}
+		if (!doc) {
+			res.status(404).send('No match for company.\n');
+			return;
+		}
+		// Found document in mongoDB, now remove it
+		
+	});
+
+});
+
 
 // ----------------- end of current assignment -------------------
 
 module.exports = api;
+
+
+
+
